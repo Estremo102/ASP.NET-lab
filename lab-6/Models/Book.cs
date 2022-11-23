a7da7108b99dc1bb9c2546e25e90008bdac9fe69
Dominik Możdżeń <dominikmozdzen@gmail.com>
2022-11-23 11:49:27 +0100

    lab 6 clone

---
+++ lab-6/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab_6.Models;
+
+public class Book
+{
+    public Book()
+    {
+        Authors = new HashSet<Author>();
+    }
+
+    [HiddenInput]
+    public int Id { get; set; }
+   
+    [Required]
+    [StringLength(200)]
+    public string Title { get; set; }
+    
+    public DateTime ReleaseDate { get; set; }
+    
+    public DateTime Created { get; set; }
+    
+    public ISet<Author> Authors { get; set; }
+}
