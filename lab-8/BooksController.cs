a9a1d114451bd0bf722e3e2127c877cc0b323145
Dominik Możdżeń <dominikmozdzen@gmail.com>
2022-12-14 16:23:34 +0100

    lab9

---
+++ lab-8/BooksController.cs
@@ -0,0 +1,67 @@
+﻿using lab_8.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace lab_8
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService service;
+        public BooksController(IBookService service)
+        {
+            this.service = service;
+        }
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get() => service.FindAll();
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        //public ActionResult<Book> Get(int id) => service.FindBy(id) == null ? NotFound() : Ok(service.FindBy(id));
+        public ActionResult<Book> Get(int id)
+        {
+            var book = service.FindBy(id);
+            if (book == null)
+                return NotFound();
+            return Ok(book);
+    }
+
+    // POST api/<BooksController>
+    [HttpPost]
+        public ActionResult<Book> Post([FromBody] Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                int id =service.Save(book);
+                return Created($"/api/books/{id}", book);
+            }
+            return BadRequest();
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public ActionResult<Book> Put(int id, [FromBody] Book book)
+        {
+            book.Id = id;
+            if (!ModelState.IsValid)
+                return BadRequest();
+            if (service.Update(book))
+                return Ok();
+            return NotFound();
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (service.Delete(id))
+                return NoContent();
+            return NotFound();
+        }
+    }
+}
--- lab-8/Program.cs
+++ lab-8/Program.cs
@@ -23,7 +23,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
